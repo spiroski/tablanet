53005cfc5c6c8a257c570663ffbac61f78c70925
deploy.sh <dimitarspiroski@Dimitars-MacBook-Pro.local>
2016-08-03 23:04:54 +0200

    publish: New build
    generated from commit af35488b53568253050bd5d5b96f7c821bbd6236

--- app/containers/App/constants.js
+++
@@ -1,3 +0,0 @@
-export const ADD_MARK = 'tablanet/App/ADD_MARK';
-export const ADD_ROUND = 'tablanet/App/ADD_ROUND';
-export const CHANGE_NAME = 'tablanet/APP/CHANGE_NAME';
--- app/containers/NewRoundInput/constants.js
+++
@@ -1,3 +0,0 @@
-export const ROUND_SCORE_CHANGE = 'app/NewRoundInput/ROUND_SCORE_CHANGE';
-export const BONUS_PLAYERID_CHANGE = 'app/NewRoundInput/BONUS_PLAYERID_CHANGE';
-export const RESET = 'app/NewRoundInput/RESET';
